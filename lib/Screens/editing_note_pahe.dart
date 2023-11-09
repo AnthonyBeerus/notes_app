adcadacb602655551b0e8f3466362abf5f47eed6
AnthonyBeerus <morima.anthony@gmail.com>
2023-11-09 18:47:40 +0200

    Function for loading Existing Note

--- lib/Screens/editing_note_pahe.dart
+++ lib/Screens/editing_note_pahe.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/Models/note.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 
 class EditingNotePage extends StatefulWidget {
   Note note;
@@ -16,8 +17,29 @@ class EditingNotePage extends StatefulWidget {
 }
 
 class _EditingNotePageState extends State<EditingNotePage> {
+  QuillController _controller = QuillController.basic();
+
+  @override
+  void initState() {
+    super.initState();
+    loadExistingNote();
+  }
+
+  //* Load Existing Note
+  void loadExistingNote() {
+    final doc = Document()..insert(0, widget.note.text);
+    setState(() {
+      _controller = QuillController(
+        document: doc, 
+        selection: const TextSelection.collapsed(offset: 0),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+    );
   }
 }
